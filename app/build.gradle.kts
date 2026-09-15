10edf0b54356062a10bea12915c8b275b9bab417
go-0dboy <84291994+go-0dboy@users.noreply.github.com>
2026-09-15 11:43:56 +0300

    build: target stable Android 16 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.github.go0dboy.articlenavigator"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.go0dboy.articlenavigator"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0-dev"
     }
