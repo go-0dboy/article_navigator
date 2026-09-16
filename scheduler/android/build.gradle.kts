507cf4e0689f29f04797ecc6363e3bd0d4a246c9
go-0dboy <84291994+go-0dboy@users.noreply.github.com>
2026-09-16 09:08:24 +0300

    Enable WorkManager Robolectric integration tests

--- scheduler/android/build.gradle.kts
+++ scheduler/android/build.gradle.kts
@@ -14,9 +14,18 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
 }
 
 dependencies {
     implementation(project(":scheduler:core"))
     implementation(libs.androidx.work.runtime.ktx)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.work.testing)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
