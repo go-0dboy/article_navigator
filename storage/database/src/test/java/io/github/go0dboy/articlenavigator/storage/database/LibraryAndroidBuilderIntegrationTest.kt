5604fa6c7421139e2303a750e382899f8a6088f3
go-0dboy <84291994+go-0dboy@users.noreply.github.com>
2026-09-16 15:15:04 +0300

    test: use Robolectric SDK compatible with project JDK

--- storage/database/src/test/java/io/github/go0dboy/articlenavigator/storage/database/LibraryAndroidBuilderIntegrationTest.kt
+++ storage/database/src/test/java/io/github/go0dboy/articlenavigator/storage/database/LibraryAndroidBuilderIntegrationTest.kt
@@ -36,7 +36,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class LibraryAndroidBuilderIntegrationTest {
     private val now = Instant.parse("2026-09-16T11:00:00Z")
 
